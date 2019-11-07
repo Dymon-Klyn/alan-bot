7e8d3c7755f9d2271050e9fb5243077875fe13e2
Daniel Martinez <danyalternate@gmail.com>
2019-11-07 07:58:19 -0500

    joke

--- Bot.py
+++ Bot.py
@@ -5,6 +5,14 @@ from discord.ext import commands
 def get_verse():
     return requests.get("http://www.ourmanna.com/verses/api/get?format=text&order=random").text.replace("God", "Alan")
 
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    header = {"Accept" : "application/json"}
+    joke = requests.get(url, headers = header).json()
+    return joke["joke"]
+
+
+
 bot = commands.Bot(command_prefix = "+")
 
 @bot.event
@@ -14,5 +22,10 @@ async def on_ready():
 @bot.command(name="alan", help = "response with a verse from the bible")
 async def send_verse(ctx):
         response = get_verse()
+        await ctx.send(response)
+
+@bot.command(name="oscar", help = "response with a joke")
+async def send_verse(ctx):
+        response = get_joke()
         await ctx.send(response)
 bot.run("NjQxOTE5NDI1MzMwNjc1NzEz.XcPYWw.oYogV-zu0X2qQ68UZWV3ls9Q2Vw")
