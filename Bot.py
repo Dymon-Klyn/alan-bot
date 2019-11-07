1133888ae54aecb30cae3e5246f83682fd811829
Daniel Martinez <danyalternate@gmail.com>
2019-11-07 05:58:46 -0500

    initial commit

---
+++ Bot.py
@@ -0,0 +1,13 @@
+import requests
+from discord.ext import commands
+
+def get_verse():
+    return requests.get("http://www.ourmanna.com/verses/api/get?format=text&order=random").text.replace("God", "Alan")
+
+bot = commands.Bot(command_prefix = "+")
+
+@bot.command(name="alan", help = "response with a verse from the bible")
+async def send_verse(ctx):
+        response = get_verse()
+        await ctx.send(response)
+bot.run("NjQxOTE5NDI1MzMwNjc1NzEz.XcPYWw.oYogV-zu0X2qQ68UZWV3ls9Q2Vw")
